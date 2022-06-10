2528c13794efffcfd1b38a6803ca7e040a776b7c
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-06-10 00:35:44 -0400

    [add] updates

--- src/main/java/org/deck/Dobble.java
+++ src/main/java/org/deck/Dobble.java
@@ -6,9 +6,34 @@ public class Dobble {
 
 
     public Dobble() {
+        List<Object> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(11);
+        list.add(12);
+
+        addCard(CreateFirstCard(list,3));
+        addCards(CreateNCards(list,3));
+        addCards(CreateNSquareCards(list,3));
+
+
+        for (int i = 0; i < DobbleCards.size(); i++) {
+          System.out.println( DobbleCards.get(i).getElements());
+        }
+
+
     }
 
-    public List<Card> DobbleCards;
+
+    public List<Card> DobbleCards = new ArrayList<>();
 
     public void isDobble() {
         // TODO implement here
@@ -18,8 +43,8 @@ public class Dobble {
         // TODO implement here
     }
 
-    public void GetNthCard() {
-        // TODO implement here
+    public Card GetNthCard(List<Card> cards, int position) {
+       return cards.get(position);
     }
 
     public void GetRequiredElements() {
@@ -31,7 +56,8 @@ public class Dobble {
     }
 
     public void ToString() {
-        // TODO implement here
+
+
     }
 
     public Card CreateFirstCard(List<Object> elements,Integer order) {
@@ -76,13 +102,22 @@ public class Dobble {
     }
 
     public Object calculateIndexToGetNCards( List<Object> elements, int order, int j, int k){
-        return elements.get(((order-1) * j + (k+1))-1);
+        return elements.get((order-1) * j + (k+1));
     }
 
     public Object calculateIndexToGetNSquareCards( List<Object> elements,int i, int order, int j, int k){
         return elements.get((order+2+order*(k-1)+(((i-1)*(k-1)+j-1) % order))-1);
     }
 
+    public void addCards(List<Card> cards){
+        for (int i = 0; i < cards.size(); i++) {
+            DobbleCards.add(GetNthCard(cards,i));
+        }
+    }
+
+    public void addCard(Card card){
+        DobbleCards.add(card);
+    }
 
 
     public void Shuffle() {
--- src/main/java/org/deck/Main.java
+++ src/main/java/org/deck/Main.java
@@ -7,27 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Object> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
-        list.add("7");
-        list.add(8);
-        list.add(9);
-        list.add(10);
-        list.add(11);
-        list.add(12);
+
 
 
 
         Dobble dobble = new Dobble();
-        Card  firstCard = dobble.CreateFirstCard(list,3);
-        System.out.println(firstCard.getElements());
-        System.out.println(dobble.CreateNCards(list,3));
-        System.out.println(dobble.CreateNSquareCards(list,3));
+
 
 
 
