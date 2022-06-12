0fb55d592e278438fc521803fe354a5c064030e0
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-06-11 21:33:47 -0400

    [refactor] ICard

--- src/main/java/model/deck/ICard.java
+++ src/main/java/model/deck/ICard.java
@@ -1,14 +1,9 @@
 package model.deck;
 
 public interface ICard {
-    public void addElement(Object element);
-
-    public Object GetFirstElement();
-
-    public Object GetLastElement();
-
-    public Boolean CompareUniqueElements(Card FirstCard);
-
-    @Override
-    public String toString();
+     void addElement(Object element);
+     Object GetFirstElement();
+     Object GetLastElement();
+     Boolean CompareUniqueElements(Card FirstCard);
+     String toString();
 }
